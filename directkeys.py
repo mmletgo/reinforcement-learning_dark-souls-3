a53e4dd153ed3b8336a59afa999d6a5b1d4bc87e
26 <96102292+Ekko1zz@users.noreply.github.com>
2024-11-02 14:56:00 +0800

    Add files via upload

--- directkeys.py
+++ directkeys.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
+"""
+Modified version for game controls
+"""
+
 import ctypes
 import time
-
-from bokeh.events import Press
+import win32api
+import win32con
 
 SendInput = ctypes.windll.user32.SendInput
 
@@ -10,89 +14,49 @@ W = 0x11
 A = 0x1E
 S = 0x1F
 D = 0x20
-
-M = 0x32
-J = 0x24
-K = 0x25
-LSHIFT = 0x2A
-LALT = 0x38
-SPACE = 0x39
-R = 0x13  # 用R代替识破
-V = 0x2F
-
-Q = 0x10
-I = 0x17
-O = 0x18
-P = 0x19
-C = 0x2E
+E = 0x12
+R = 0x13
 F = 0x21
+Q = 0x10
+ALT = 0x38
+SPACE = 0x39
+LSHIFT = 0x2A
 
-up = 0xC8
-down = 0xD0
-left = 0xCB
-right = 0xCD
+UP = 0xC8
+DOWN = 0xD0
+LEFT = 0xCB
+RIGHT = 0xCD
 
-mouse_left = [0x0002, 0x0004]
-mouse_right = [0x0008, 0x0010]
-
-esc = 0x01
-
-# C struct redefinitions
+# C struct redefinitions 
 PUL = ctypes.POINTER(ctypes.c_ulong)
-
-
 class KeyBdInput(ctypes.Structure):
-    _fields_ = [("wVk", ctypes.c_ushort), ("wScan", ctypes.c_ushort),
-                ("dwFlags", ctypes.c_ulong), ("time", ctypes.c_ulong),
+    _fields_ = [("wVk", ctypes.c_ushort),
+                ("wScan", ctypes.c_ushort),
+                ("dwFlags", ctypes.c_ulong),
+                ("time", ctypes.c_ulong),
                 ("dwExtraInfo", PUL)]
 
-
 class HardwareInput(ctypes.Structure):
-    _fields_ = [("uMsg", ctypes.c_ulong), ("wParamL", ctypes.c_short),
+    _fields_ = [("uMsg", ctypes.c_ulong),
+                ("wParamL", ctypes.c_short),
                 ("wParamH", ctypes.c_ushort)]
 
-
 class MouseInput(ctypes.Structure):
-    _fields_ = [("dx", ctypes.c_long), ("dy", ctypes.c_long),
-                ("mouseData", ctypes.c_ulong), ("dwFlags", ctypes.c_ulong),
-                ("time", ctypes.c_ulong), ("dwExtraInfo", PUL)]
-
+    _fields_ = [("dx", ctypes.c_long),
+                ("dy", ctypes.c_long),
+                ("mouseData", ctypes.c_ulong),
+                ("dwFlags", ctypes.c_ulong),
+                ("time",ctypes.c_ulong),
+                ("dwExtraInfo", PUL)]
 
 class Input_I(ctypes.Union):
-    _fields_ = [("ki", KeyBdInput), ("mi", MouseInput), ("hi", HardwareInput)]
-
+    _fields_ = [("ki", KeyBdInput),
+                 ("mi", MouseInput),
+                 ("hi", HardwareInput)]
 
 class Input(ctypes.Structure):
-    _fields_ = [("type", ctypes.c_ulong), ("ii", Input_I)]
-
-class POINT(ctypes.Structure):
-    _fields_ = [("x", ctypes.c_long), ("y", ctypes.c_long)]
-
-
-
-# Actuals Functions
-
-def get_mouse_position():
-    pt = POINT()
-    ctypes.windll.user32.GetCursorPos(ctypes.byref(pt))
-    return pt.x, pt.y
-
-def get_screen_center():
-    user32 = ctypes.windll.user32
-    screen_width = user32.GetSystemMetrics(0)
-    screen_height = user32.GetSystemMetrics(1)
-    center_x = screen_width // 2
-    center_y = screen_height // 2
-    return center_x, center_y
-
-def ClickMouse(mouseCode):
-    x, y = get_mouse_position()
-    extra = ctypes.c_ulong(0)
-    ii_ = Input_I()
-    ii_.mi = MouseInput(x, y, 0, mouseCode, 0, ctypes.pointer(extra))
-    x = Input(ctypes.c_ulong(1), ii_)
-    ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
-
+    _fields_ = [("type", ctypes.c_ulong),
+                ("ii", Input_I)]
 
 def PressKey(hexKeyCode):
     extra = ctypes.c_ulong(0)
@@ -101,214 +65,181 @@ def PressKey(hexKeyCode):
     x = Input(ctypes.c_ulong(1), ii_)
     ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
 
-
 def ReleaseKey(hexKeyCode):
     extra = ctypes.c_ulong(0)
     ii_ = Input_I()
-    ii_.ki = KeyBdInput(0, hexKeyCode, 0x0008 | 0x0002, 0,
-                        ctypes.pointer(extra))
+    ii_.ki = KeyBdInput(0, hexKeyCode, 0x0008 | 0x0002, 0, ctypes.pointer(extra))
     x = Input(ctypes.c_ulong(1), ii_)
     ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
 
+def left_click():
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
+    time.sleep(0.1)
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0, 0, 0)
 
-def shield():
-    ClickMouse(mouse_right[0])
-    time.sleep(0.5)
-    ClickMouse(mouse_right[1])
+def right_click():
+    win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTDOWN, 0, 0, 0, 0)
+    time.sleep(0.1)
+    win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTUP, 0, 0, 0, 0)
 
-def shield_heavy():
+def attack_right():
+    left_click()
+
+def heavy_attack_right():
     PressKey(LSHIFT)
-    time.sleep(0.1)
-    ClickMouse(mouse_right[0])
-    time.sleep(0.1)
-    ClickMouse(mouse_right[1])
+    left_click()
     ReleaseKey(LSHIFT)
 
-def sword_light():
-    ClickMouse(mouse_left[0])
-    time.sleep(0.1)
-    ClickMouse(mouse_left[1])
+def attack_left():
+    right_click()
 
-def sword_heavy():
+def heavy_attack_left():
     PressKey(LSHIFT)
-    time.sleep(0.1)
-    ClickMouse(mouse_left[0])
-    time.sleep(0.1)
-    ClickMouse(mouse_left[1])
+    right_click()
     ReleaseKey(LSHIFT)
-
-def jump_backward():
-    PressKey(SPACE)
-    time.sleep(0.1)
-    ReleaseKey(SPACE)
-
-def jump_forward():
-    PressKey(W)
-    time.sleep(0.1)
-    PressKey(SPACE)
-    time.sleep(0.1)
-    ReleaseKey(SPACE)
-    ReleaseKey(W)
-
-def jump_left():
-    PressKey(A)
-    time.sleep(0.1)
-    PressKey(SPACE)
-    time.sleep(0.1)
-    ReleaseKey(SPACE)
-    ReleaseKey(A)
-
-def jump_right():
-    PressKey(D)
-    time.sleep(0.1)
-    PressKey(SPACE)
-    time.sleep(0.1)
-    ReleaseKey(SPACE)
-    ReleaseKey(D)
 
 def use_item():
     PressKey(R)
     time.sleep(0.1)
     ReleaseKey(R)
 
-def lock_target():
+def action():
+    PressKey(E)
+    time.sleep(0.1)
+    ReleaseKey(E)
+
+def two_hand_weapon():
+    PressKey(F)
+    time.sleep(0.1)
+    ReleaseKey(F)
+
+def toggle_walk_run():
+    PressKey(ALT)
+    time.sleep(0.1)
+    ReleaseKey(ALT)
+
+def run_forward():
+    PressKey(W)
+    time.sleep(0.1)
+
+def stop_forward():
+    ReleaseKey(W)
+
+def run_backward():
+    PressKey(S)
+    time.sleep(0.1)
+
+def stop_backward():
+    ReleaseKey(S)
+
+def run_left():
+    PressKey(A)
+    time.sleep(0.1)
+
+def stop_left():
+    ReleaseKey(A)
+
+def run_right():
+    PressKey(D)
+    time.sleep(0.1)
+
+def stop_right():
+    ReleaseKey(D)
+
+def sprint_jump_roll():
+    PressKey(SPACE)
+    time.sleep(0.1)
+    ReleaseKey(SPACE)
+
+def reset_camera():
     PressKey(Q)
     time.sleep(0.1)
     ReleaseKey(Q)
 
-def move_mouse_to_boss(bx, by):
-    center_x, center_y = get_screen_center()
-    dx = bx - center_x
-    dy = by - center_y
-    ctypes.windll.user32.mouse_event(0x0001, dx, dy, 0, 0)
-
-def defense():
-    PressKey(M)
-    time.sleep(0.05)
-    ReleaseKey(M)
-    # time.sleep(0.1)
-
-
-def attack():
-    PressKey(J)
-    time.sleep(0.05)
-    ReleaseKey(J)
-    # time.sleep(0.1)
-
-
-def go_forward():
-    PressKey(W)
-    time.sleep(0.4)
-    ReleaseKey(W)
-
-
-def go_back():
-    PressKey(S)
-    time.sleep(0.4)
-    ReleaseKey(S)
-
-
-def go_left():
-    PressKey(A)
-    time.sleep(0.4)
-    ReleaseKey(A)
-
-
-def go_right():
-    PressKey(D)
-    time.sleep(0.4)
-    ReleaseKey(D)
-
-
-def jump():
-    PressKey(K)
+def switch_spell():
+    PressKey(UP)
     time.sleep(0.1)
-    ReleaseKey(K)
-    # time.sleep(0.1)
+    ReleaseKey(UP)
 
-
-def dodge():  # 闪避
-    PressKey(R)
+def switch_item():
+    PressKey(DOWN)
     time.sleep(0.1)
-    ReleaseKey(R)
-    # time.sleep(0.1)
+    ReleaseKey(DOWN)
 
-
-def lock_vision():
-    PressKey(V)
-    time.sleep(0.3)
-    ReleaseKey(V)
+def switch_left_weapon():
+    PressKey(LEFT)
     time.sleep(0.1)
+    ReleaseKey(LEFT)
+
+def switch_right_weapon():
+    PressKey(RIGHT)
+    time.sleep(0.1)
+    ReleaseKey(RIGHT)
 
 
-def go_forward_QL(t):
-    PressKey(W)
-    time.sleep(t)
-    ReleaseKey(W)
+# if __name__ == '__main__':
+#     time.sleep(5)  # 等待5秒
+#     time1 = time.time()
+#     while(True):
+#         if abs(time.time()-time1) > 5:
+#             break
+#         else:
+#             PressKey(W)
+#             time.sleep(0.1)
+#             ReleaseKey(W)
+#             time.sleep(0.2)
 
 
-def turn_left(t):
-    PressKey(left)
-    time.sleep(t)
-    ReleaseKey(left)
 
-
-def turn_up(t):
-    PressKey(up)
-    time.sleep(t)
-    ReleaseKey(up)
-
-
-def turn_right(t):
-    PressKey(right)
-    time.sleep(t)
-    ReleaseKey(right)
-
-
-def F_go():
-    PressKey(F)
-    time.sleep(0.5)
-    ReleaseKey(F)
-
-
-def forward_jump(t):
-    PressKey(W)
-    time.sleep(t)
-    PressKey(K)
-    ReleaseKey(W)
-    ReleaseKey(K)
-
-
-def press_esc():
-    PressKey(esc)
-    time.sleep(0.3)
-    ReleaseKey(esc)
-
-
-def dead():
-    PressKey(M)
-    time.sleep(0.5)
-    ReleaseKey(M)
-
+def test_controls():
+    print("控制测试程序启动...")
+    print("请确保您在5秒内切换到目标窗口")
+    time.sleep(5)
+    
+    def test_action(action_func, action_name):
+        print(f"测试 {action_name}...")
+        action_func()
+        time.sleep(0.5)  
+        
+    # 测试方案
+    test_sequences = [
+        # 基础攻击测试
+        (attack_right, "右手普通攻击"),
+        (heavy_attack_right, "右手重击"),
+        (attack_left, "左手普通攻击"),
+        (heavy_attack_left, "左手重击"),
+        
+        # 移动测试
+        (lambda: (run_forward(), time.sleep(1), stop_forward()), "向前移动1秒"),
+        (lambda: (run_backward(), time.sleep(1), stop_backward()), "向后移动1秒"),
+        (lambda: (run_left(), time.sleep(1), stop_left()), "向左移动1秒"),
+        (lambda: (run_right(), time.sleep(1), stop_right()), "向右移动1秒"),
+        
+        # 功能键测试
+        (use_item, "使用物品"),
+        (action, "互动"),
+        (two_hand_weapon, "双手持武器"),
+        (sprint_jump_roll, "翻滚"),
+        (reset_camera, "重置镜头"),
+        
+        # 切换测试
+        (switch_spell, "切换法术"),
+        (switch_item, "切换物品"),
+        (switch_left_weapon, "切换左手武器"),
+        (switch_right_weapon, "切换右手武器")
+    ]
+    
+    try:
+        for action_func, action_name in test_sequences:
+            test_action(action_func, action_name)
+            # response = input(f"{action_name} 测试完成。按Enter继续下一个测试，输入'q'退出: ")
+            # if response.lower() == 'q':
+            #     break
+                
+    except KeyboardInterrupt:
+        print("\n测试被用户中断")
+    finally:
+        print("测试程序结束")
 
 if __name__ == '__main__':
-    time.sleep(5)
-    time1 = time.time()
-    while (True):
-        if abs(time.time() - time1) > 5:
-            break
-        else:
-            PressKey(M)
-            time.sleep(0.1)
-            ReleaseKey(M)
-            time.sleep(0.2)
-
-    PressKey(W)
-    time.sleep(0.4)
-    ReleaseKey(W)
-    time.sleep(1)
-
-    PressKey(J)
-    time.sleep(0.1)
-    ReleaseKey(J)
-    time.sleep(1)
+    test_controls()
--- utility.py
+++ utility.py
@@ -3,6 +3,7 @@ import time
 import directkeys
 
 
+
 def pause_game(paused):
     keys = key_check()
     if 'T' in keys:
@@ -31,37 +32,185 @@ def pause_game(paused):
     return paused
 
 
-# calculate self blood
-# TODO: need to be modified
-def self_blood_count(self_gray):
+
+###################################################################################################################
+'''
+def self_blood_count(color_image, red_self_blood_threshold=80,green_self_blood_threshold = 80,blue_self_blood_threshold =80 ):
     self_blood = 0
-    for self_bd_num in self_gray[469]:
-        # self blood gray pixel 80~98
-        if self_bd_num > 90 and self_bd_num < 98:
+    for pixel in color_image[4]:
+        red_value = pixel[2]
+        green_value = pixel[1]
+        blue_value = pixel[0]
+        if red_value > red_self_blood_threshold and green_value < green_self_blood_threshold and blue_value < green_self_blood_threshold:
             self_blood += 1
-    return self_blood
 
+    total_pixels = color_image.shape[1]
+    health_percentage = (self_blood / total_pixels) * 100
 
-# calculate boss blood
-# TODO: need to be modified
-def boss_blood_count(boss_gray):
-    boss_blood = 0
-    for boss_bd_num in boss_gray[0]:
-        # boss blood gray pixel 65~75
-        if boss_bd_num > 65 and boss_bd_num < 75:
-            boss_blood += 1
-    return boss_blood
+    return health_percentage
 
+def boss_blood_count(color_image, red_boss_blood_threshold=70, self_stamina_green=30, self_stamina_blue=30):
+    self_blood = 0
+    for pixel in color_image[4]:
+        red_value = pixel[2]
+        green_value = pixel[1]
+        blue_value = pixel[0]
 
+        if red_value > red_boss_blood_threshold and green_value < self_stamina_green and blue_value < self_stamina_blue:
+            self_blood += 1
+
+    total_pixels = color_image.shape[1]
+    health_percentage = (self_blood / total_pixels) * 100
+
+    return health_percentage
+
+def self_stamina_count(color_image, self_stamina_red=80, self_stamina_green=110, self_stamina_blue=80):
+    self_blood = 0
+    for pixel in color_image[4]:
+        red_value = pixel[2]
+        green_value = pixel[1]
+        blue_value = pixel[0]
+
+        if red_value > self_stamina_red and green_value > self_stamina_green and blue_value > self_stamina_blue:
+            self_blood += 1
+
+    total_pixels = color_image.shape[1]
+    health_percentage = (self_blood / total_pixels) * 100
+
+    return health_percentage
+'''
+'''
+def self_blood_count(color_image, red_self_blood_threshold=80, green_self_blood_threshold=80, blue_self_blood_threshold=80):
+    try:
+        if len(color_image.shape) != 3:
+            print(f"Warning: Image should be 3 channels, but got shape {color_image.shape}")
+            return 0
+            
+        self_blood = 0
+        height = color_image.shape[0]
+        width = color_image.shape[1]
+        
+        row = 4  
+        if row >= height:
+            print(f"Warning: Row 4 is out of bounds. Image height is {height}")
+            return 0
+            
+        for x in range(width):
+            pixel = color_image[row, x]  
+            blue_value = pixel[0]
+            green_value = pixel[1]
+            red_value = pixel[2]
+            
+            if (red_value > red_self_blood_threshold and 
+                green_value < green_self_blood_threshold and 
+                blue_value < blue_self_blood_threshold):
+                self_blood += 1
+
+        health_percentage = (self_blood / width) * 100
+        return health_percentage
+        
+    except Exception as e:
+        print(f"Error in self_blood_count: {e}")
+        print(f"Image shape: {color_image.shape}")
+        return 0
+
+def boss_blood_count(color_image, red_boss_blood_threshold=70, self_stamina_green=30, self_stamina_blue=30):
+    try:
+        if len(color_image.shape) != 3:
+            print(f"Warning: Image should be 3 channels, but got shape {color_image.shape}")
+            return 0
+            
+        self_blood = 0
+        height = color_image.shape[0]
+        width = color_image.shape[1]
+        
+        row = 4
+        if row >= height:
+            print(f"Warning: Row 4 is out of bounds. Image height is {height}")
+            return 0
+            
+        for x in range(width):
+            pixel = color_image[row, x]
+            blue_value = pixel[0]
+            green_value = pixel[1]
+            red_value = pixel[2]
+
+            if (red_value > red_boss_blood_threshold and 
+                green_value < self_stamina_green and 
+                blue_value < self_stamina_blue):
+                self_blood += 1
+
+        health_percentage = (self_blood / width) * 100
+        return health_percentage
+        
+    except Exception as e:
+        print(f"Error in boss_blood_count: {e}")
+        print(f"Image shape: {color_image.shape}")
+        return 0
+
+def self_stamina_count(color_image, self_stamina_red=80, self_stamina_green=110, self_stamina_blue=80):
+    try:
+        if len(color_image.shape) != 3:
+            print(f"Warning: Image should be 3 channels, but got shape {color_image.shape}")
+            return 0
+            
+        self_blood = 0
+        height = color_image.shape[0]
+        width = color_image.shape[1]
+        
+        row = 4
+        if row >= height:
+            print(f"Warning: Row 4 is out of bounds. Image height is {height}")
+            return 0
+            
+        for x in range(width):
+            pixel = color_image[row, x]
+            blue_value = pixel[0]
+            green_value = pixel[1]
+            red_value = pixel[2]
+
+            if (red_value > self_stamina_red and 
+                green_value > self_stamina_green and 
+                blue_value > self_stamina_blue):
+                self_blood += 1
+
+        health_percentage = (self_blood / width) * 100
+        return health_percentage
+        
+    except Exception as e:
+        print(f"Error in self_stamina_count: {e}")
+        print(f"Image shape: {color_image.shape}")
+        return 0
+'''
+
+#############################################################################################################################
 # TODO: need to be modified
 def take_action(action):
     if action == 0:  # n_choose
         pass
-    elif action == 1:  # j
-        directkeys.attack()
-    elif action == 2:  # k
-        directkeys.jump()
-    elif action == 3:  # m
-        directkeys.defense()
-    elif action == 4:  # r
-        directkeys.dodge()
+    elif action == 1:  # 左击
+        directkeys.left_click()
+    elif action == 2:  # 右击（盾）
+        directkeys.right_click()
+    elif action == 3:  # 重击
+        directkeys.heavy_attack_left()
+    elif action == 4:  # r喝药
+        directkeys.use_item()
+    elif action == 5:  # 向后闪避，没加翻滚
+        directkeys.sprint_jump_roll()
+    elif action == 6:  # 往前走w
+        directkeys.run_forward()
+        time.sleep(1)
+        directkeys.stop_forward()
+    elif action == 7:  # 往后走s
+        directkeys.run_backward()
+        time.sleep(1)
+        directkeys.stop_backward()
+    elif action == 8:  # 往左走a
+        directkeys.run_left()
+        time.sleep(1)
+        directkeys.stop_left()
+    elif action == 9:  # 往右走d
+        directkeys.run_right()
+        time.sleep(1)
+        directkeys.stop_right()
